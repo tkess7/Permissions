19a69b93608a06b60699e00255f409444b7c0ce2
Travis <tkess17@gmail.com>
2024-06-01 17:02:34 -0500

    Initial Commit

---
+++ Permissions/PermissionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PermissionsApp.swift
+//  Permissions
+//
+//  Created by Travis Kessinger on 6/1/24.
+//
+
+import SwiftUI
+
+@main
+struct PermissionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
