83cc252a18d9bb9499391dfe4d16a453f5163b8e
Travis <tkess17@gmail.com>
2024-06-01 19:33:33 -0500

    Use State property to manage enabled permissions state

--- Permissions/HomeView.swift
+++ Permissions/HomeView.swift
@@ -9,17 +9,21 @@ import SwiftUI
 
 struct HomeView: View {
     @State private var presentGetStartedView = false
+    @State private var allPermissionsEnabled = false
     var body: some View {
         VStack(spacing: 24) {
             Text("Home View")
             
-            if !PermissionsUtility.shared.allPermissionsEnabled {
+            if !allPermissionsEnabled {
                 Button("Present full screen cover") {
                     presentGetStartedView = true
                 }
             } else {
                 Text("All permissions are enabled!")
             }
+        }
+        .onReceive(PermissionsUtility.shared.$allPermissionsEnabled) { allPermissionsEnabled in
+            self.allPermissionsEnabled = allPermissionsEnabled
         }
         .fullScreenCover(isPresented: $presentGetStartedView) {
             GetStartedView(showPermissionsFlow: $presentGetStartedView)
